9dd8c075873411189f1eac952e92025577e9f21f
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-11-02 22:13:31 +0100

    commit

--- src/main/java/dev/elma/DataMapper.java
+++ src/main/java/dev/elma/DataMapper.java
@@ -5,15 +5,27 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.*;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataMapper extends Mapper<LongWritable, Text,Text, DoubleWritable[]> {
+    BufferedReader bufferedReader;
+
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, DoubleWritable[]>.Context context) throws IOException, InterruptedException {
+        List<Double[]> centers = readCenters();
+        String[] s = value.toString().split(" ");
+        double x = Double.parseDouble(s[0]);
+        double y = Double.parseDouble(s[1]);
+        centers.forEach(c->{
+            double X = Math.pow(x - c[0], 2);
+            double Y = Math.pow(y - c[1], 2);
+        });
+
 
     }
 
@@ -30,5 +42,15 @@ public class DataMapper extends Mapper<LongWritable, Text,Text, DoubleWritable[]
             }
 
         }
+    }
+
+    public List<Double[]> readCenters() throws IOException {
+        String line;
+        List<Double[]> centers=new ArrayList<Double[]>();
+        while ((line=bufferedReader.readLine())!=null){
+            String[] s = line.split(" ");
+            centers.add(new Double[]{Double.parseDouble(s[0]),Double.parseDouble(s[1])});
+        }
+        return centers;
     }
 }
